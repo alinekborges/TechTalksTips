2bafcc5e322ffedf5467531c8cc28153653416c7
Aline Borges <aline.borges@uber.com>
2019-10-31 12:37:37 -0300

    Add tips and detail screen

--- TechTalkTips/FeaturesGroup/Tabs/Tips/TipsViewController.swift
+++ TechTalkTips/FeaturesGroup/Tabs/Tips/TipsViewController.swift
@@ -13,7 +13,9 @@ class TipsViewController: UIViewController {
     
     let service: Servicing
     
-    let internalView = UIView()
+    private let internalView = TipsView()
+    private var tips: [Tip] = []
+    
     lazy var stackView: UIStackView = {
         let stack = UIStackView()
         stack.distribution = .fill
@@ -34,28 +36,38 @@ class TipsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
+        internalView.tableView.dataSource = self
+        internalView.tableView.delegate = self
+        internalView.tableView.register(TipCell.self, forCellReuseIdentifier: TipsView.identifier)
+        fetchData()
         
-        let shareActionFeaturePoint = ShareActionFeaturePoint(cachedExperiments: FakeExperiments())
-        let actions = shareActionFeaturePoint.createFeatures()
-        actions.forEach(self.addChild)
+        //self.view.backgroundColor = .white
         
-        self.view.addSubview(stackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
+//        let shareActionFeaturePoint = ShareActionFeaturePoint(cachedExperiments: FakeExperiments())
+//        let actions = shareActionFeaturePoint.createFeatures()
+//        actions.forEach(self.addChild)
+//
+//        self.view.addSubview(stackView)
+//        stackView.translatesAutoresizingMaskIntoConstraints = false
+//
+//        stackView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100).isActive = true
+//        //stackView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+//        stackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+//        stackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+//
+//        actions.compactMap { $0.view }.forEach { view in
+//            view.translatesAutoresizingMaskIntoConstraints = false
+//            view.heightAnchor.constraint(equalToConstant: 100.0).isActive = true
+//            stackView.addArrangedSubview(view)
+//        }
         
-        stackView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100).isActive = true
-        //stackView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-        stackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        stackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
         
-        actions.compactMap { $0.view }.forEach { view in
-            view.translatesAutoresizingMaskIntoConstraints = false
-            view.heightAnchor.constraint(equalToConstant: 100.0).isActive = true
-            stackView.addArrangedSubview(view)
-        }
-        
-        service.getTips { tips in
-            print(tips)
+    }
+    
+    func fetchData() {
+        service.getTips { [weak self] tips in
+            self?.tips = tips
+            self?.internalView.tableView.reloadData()
         }
     }
     
@@ -65,69 +77,59 @@ class TipsViewController: UIViewController {
     
 }
 
-class ShareViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = .red
-        
-        let button = UIButton()
-        self.view.addSubview(button)
-        button.setImage(UIImage(systemName: "gear"), for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-        button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        
-        button.addTarget(self, action: #selector(self.goSomewhere), for: .touchUpInside)
+extension TipsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tips.count
     }
-            
-    @objc func goSomewhere() {
-        let destination = CommentViewController()
-        self.navigationController?.pushViewController(destination, animated: true)
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TipsView.identifier) as! TipCell
+        let tip = self.tips[indexPath.row]
+        cell.setText(tip.title)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selected = self.tips[indexPath.row]
+        let detailsController = DetailsViewController(tip: selected)
+        self.navigationController?.pushViewController(detailsController, animated: true)
     }
 }
 
-class CommentViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = .blue
-    }
-}
-
-class ShareActionFeatureFactory: FeatureFactory<UIViewController> {
+fileprivate class TipsView: UIView {
+    
+    static var identifier = String(describing: TipsView.self)
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 100
+        tableView.separatorStyle = .none
+        return tableView
+    }()
     
     init() {
-        super.init {
-            return ShareViewController()
-        }
+        super.init(frame: .zero)
+        configure()
     }
     
-    override func isApplicable(experiments: CachedExperimenting) -> Bool {
-        return experiments.isOn(Experiments.settings)
-    }
-}
-
-class CommentActionFeatureFactory: FeatureFactory<UIViewController> {
-    
-    init() {
-        super.init {
-            return CommentViewController()
-        }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
-    override func isApplicable(experiments: CachedExperimenting) -> Bool {
-        return experiments.isOn(Experiments.settings)
+    private func configure() {
+        build()
+        setupConstraints()
     }
-}
-
-
-class ShareActionFeaturePoint: FeaturePoint<UIViewController> {
     
-    override var featureFactories: [FeatureFactory<UIViewController>] {
-        return [ShareActionFeatureFactory(),
-                CommentActionFeatureFactory()]
+    private func build() {
+        self.backgroundColor = .systemBackground
+        self.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        tableView.prepareForConstraints()
+        tableView.pinEdgesToSuperview()
     }
     
 }
-
